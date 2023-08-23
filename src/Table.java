05de2da484e2131394bd53478e29aee55e0c2d5a
yohannesTz <yohannes222ethiopia@gmail.com>
2023-08-23 17:11:02 +0300

    added default value

--- src/Table.java
+++ src/Table.java
@@ -6,5 +6,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Table {
-    String name() default "";
+    String name() default "SQLTable";
 }
